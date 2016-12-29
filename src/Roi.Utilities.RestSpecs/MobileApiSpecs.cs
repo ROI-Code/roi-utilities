a9cbfe2f0bb13b4978232598d9680a9d9c8fdf5a
Fernando Cardenas <fernando@roicode.com>
2016-12-28 20:21:08 -0700

    Stated code for testing the Rest Client

---
+++ src/Roi.Utilities.RestSpecs/MobileApiSpecs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roi.Utilities.RestSpecs
+{
+    public class When_using_the_rest_client_to_against_the_Mobile_API_for_interacting_with_photos
+    {
+
+    }
+}
