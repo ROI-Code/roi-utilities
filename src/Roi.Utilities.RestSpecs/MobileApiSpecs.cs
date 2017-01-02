9101fbd610b52ab3418080f9105a3f905a520df8
Fernando Cardenas <fernando@roicode.com>
2017-01-01 19:12:18 -0700

    Small refactor to prepare for new API methods that return the objects in their entirety

--- src/Roi.Utilities.RestSpecs/MobileApiSpecs.cs
+++ src/Roi.Utilities.RestSpecs/MobileApiSpecs.cs
@@ -20,15 +20,12 @@ namespace Roi.Utilities.RestSpecs
         [Test]
         public void _010_we_should_be_able_to_create_a_new_photo()
         {
-            var newPhoto = new PhotoModel()
-            {
-                PhotoId = null,
-                UserId = Guid.NewGuid(),
-                LocationUrl = "http://www.domain.com/somelocation",
-                LongDescription = "Some longer description",
-                CreatedDate = null
-            };
-            var response = RestClientToTest.Post<PhotoModel>(ResponseFormat.Json, "photo", newPhoto, null);
+            var newPhoto = GetNew();
+            var response = RestClientToTest.Post<PhotoModel>(ResponseFormat.Json, "photo", newPhoto);
+
+            var newlyCreatedPhoto = response.ReturnedObject;
+
+            Assert.That(newlyCreatedPhoto.PhotoId, Is.Not.Null);
         }
 
         [Test]
@@ -39,7 +36,26 @@ namespace Roi.Utilities.RestSpecs
         }
 
         [Test]
-//        [Ignore("Used to clear out the database for the rest service")]
+        public void _030_we_should_be_able_to_update_a_photo()
+        {
+            var newPhoto = GetNew();
+            var response = RestClientToTest.Post<PhotoModel>(ResponseFormat.Json, "photo", newPhoto);
+
+            var photoToModify = response.ReturnedObject;
+
+            var newLocation = "http://www.url.com/NEWlocation";
+            photoToModify.LocationUrl = newLocation;
+
+            response = RestClientToTest.Put<PhotoModel>(ResponseFormat.Json, "photo", photoToModify);
+
+            var modifiedPhoto = response.ReturnedObject;
+            Assert.That(modifiedPhoto.LocationUrl, Is.EqualTo(newLocation));
+        }
+
+
+
+        [Test]
+        [Ignore("Used to clear out the database for the rest service")]
         public void _998_we_should_be_able_to_clear_out_the_data_for_the_rest_service()
         {
             var response = RestClientToTest.GetMany<PhotoModel>(ResponseFormat.Json, "photo", "");
@@ -48,16 +64,33 @@ namespace Roi.Utilities.RestSpecs
                 var deleteResponse = RestClientToTest.Delete(ResponseFormat.Json, $"photo/{photoModel.PhotoId}");
                 Assert.That(deleteResponse.Success, Is.True);
             }
+            make_sure_there_are_no_photos_returned_from_the_web_service();
+        }
 
+        [Test]
+        [Ignore("Used to make sure the web service returns no new photos")]
+        public void _999_there_should_be_no_photos_in_the_web_service()
+        {
+            make_sure_there_are_no_photos_returned_from_the_web_service();
+        }
+
+        private static void make_sure_there_are_no_photos_returned_from_the_web_service()
+        {
             var getAllResponse = RestClientToTest.GetMany<PhotoModel>(ResponseFormat.Json, "photo", "");
             Assert.That(getAllResponse.ReturnedObject.Count, Is.EqualTo(0));
         }
 
-        [Test]
-        [Ignore("Used when running after clearing out the database used by the rest service")]
-        public void _999_there_should_be_no_photos_in_the_web_service()
+        private static PhotoModel GetNew()
         {
-
+            var newPhoto = new PhotoModel()
+            {
+                PhotoId = null,
+                UserId = Guid.NewGuid(),
+                LocationUrl = "http://www.domain.com/somelocation",
+                LongDescription = "Some longer description",
+                CreatedDate = null
+            };
+            return newPhoto;
         }
     }
 }
